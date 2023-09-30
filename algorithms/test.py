2e84d17154bf627f92791d239476764098c2c16d
kalharko <oscar.dewasmes@gmail.com>
2023-09-30 22:35:02 +0200

    :tada: first test algorithm

---
+++ algorithms/test.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append(sys.path[0].rstrip(sys.path[0].split('/')[-1]))
+
+from back.pacman_game import PacmanGame
+from back.direction import Direction
+from back.action import Action
+
+from front.cli.cli_replay import CliReplay
+
+
+game = PacmanGame()
+game.load('maps/5x5.txt')
+
+game.step([Action('Pa', Direction['LEFT'])])
+game.step([Action('Pa', Direction['LEFT'])])
+game.step([Action('Pa', Direction['UP'])])
+game.step([Action('Pa', Direction['RIGHT'])])
+game.step([Action('Pa', Direction['RIGHT'])])
+game.step([Action('Pa', Direction['UP'])])
+game.step([Action('Pa', Direction['LEFT'])])
+game.step([Action('Pa', Direction['LEFT'])])
+
+replay = CliReplay(game.get_replay())
