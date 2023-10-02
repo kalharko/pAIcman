196092f86b9cd08171e21932b58a84b747cef357
kalharko <oscar.dewasmes@gmail.com>
2023-10-03 01:10:33 +0200

    :lipstick: improved UI

--- front/cli/cli_replay.py
+++ front/cli/cli_replay.py
@@ -2,17 +2,17 @@ from curses import wrapper
 import os.path as os_path
 import curses
 import locale
+from back.cell import Cell
 
 from back.pacman_game import PacmanGame
 from back.action import Action
-from back.direction import Direction
 from back.pacman import Pacman
-from back.ghost import Ghost
 
 
 class CliReplay():
     _path_board: str
     _history: list[list[Action]]
+    _fancy_walls: list[list[str]]
 
     def __init__(self, replay) -> None:
         assert isinstance(replay, tuple)
@@ -27,6 +27,7 @@ class CliReplay():
 
         self._path_board = replay[0]
         self._history = replay[1]
+        self._fancy_walls = [[]]
         if input('Start curses replay ? (Y/n)') == 'n':
             return
         wrapper(self._start)
@@ -40,6 +41,9 @@ class CliReplay():
         self._game = PacmanGame()
         self._game.load(self._path_board)
 
+        # load fancy walls
+        self._fancy_walls = self._load_fancy_walls()
+
         # encoding
         locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
         curses.curs_set(0)
@@ -52,6 +56,8 @@ class CliReplay():
         # colors
         curses.init_pair(1, curses.COLOR_BLUE, 0)  # wall
         curses.init_pair(2, curses.COLOR_YELLOW, 0)  # pacman
+        curses.init_color(700, 150, 150, 150)  # define gray
+        curses.init_pair(3, 700, 0)  # dots
 
         # main loop
         self.main_loop()
@@ -78,15 +84,19 @@ class CliReplay():
 
         # board
         cells, agents = self._game.gather_cli_state()
-        char_cell = [' ', '#', '+', '.', '0']
-        color_cell = [0, 1, 0, 0, 0]
+        char_cell = [' ', '#', '_', '·', 'Ø']
+        color_cell = [0, 1, 3, 3, 3]
         char_pacman = ['v', '<', '^', '>']
 
         for x in range(len(cells)):
             for y in range(len(cells[0])):
+                if cells[x][y] == Cell['WALL']:
+                    char = self._fancy_walls[x][y]
+                else:
+                    char = char_cell[cells[x][y].value]
                 self._screen.addstr(y + 1,
                                     x + 1,
-                                    char_cell[cells[x][y].value],
+                                    char,
                                     curses.color_pair(color_cell[cells[x][y].value]))
 
         # agents
@@ -97,3 +107,78 @@ class CliReplay():
                                     x + 1,
                                     char_pacman[agent.get_last_direction().value],
                                     curses.color_pair(2))
+
+        # score
+        i = 0
+        self._screen.addstr(2, len(cells) + 2, 'Scores', curses.color_pair(3))
+        for agent in agents:
+            if isinstance(agent, Pacman):
+                self._screen.addstr(3 + i, len(cells) + 2, f'{agent.get_id()} : {agent.get_score()}', curses.color_pair(3))
+                i += 1
+
+    def _load_fancy_walls(self) -> list[list[str]]:
+        cells, agents = self._game.gather_cli_state()
+        out = [[' ' for y in range(len(cells[0]))] for x in range(len(cells))]
+        wall_map = {str((0, 0, 0, 0, 0, 0, 0, 0)): '█',
+                    str((1, 1, 1, 1, 1, 1, 1, 1)): ' ',
+                    str((0, 0, 0, 1, 0, 1, 0, 0)): '╔',
+                    str((0, 0, 0, 1, 0, 0, 0, 1)): '═',
+                    str((0, 1, 0, 0, 0, 1, 0, 0)): '║',
+                    str((0, 0, 0, 0, 0, 1, 0, 1)): '╗',
+                    str((0, 1, 0, 0, 0, 0, 0, 1)): '╝',
+                    str((0, 1, 0, 1, 0, 0, 0, 0)): '╚',
+                    str((0, 0, 0, 1, 0, 0, 1, 1)): '═',
+                    str((0, 1, 1, 0, 0, 1, 0, 0)): '║',
+                    str((0, 0, 0, 1, 1, 0, 0, 1)): '═',
+                    str((1, 0, 0, 1, 0, 0, 0, 1)): '═',
+                    str((0, 0, 1, 1, 0, 0, 0, 1)): '═',
+                    str((0, 0, 0, 1, 0, 0, 0, 0)): '═',
+                    str((0, 0, 0, 0, 0, 0, 0, 1)): '═',
+                    str((0, 1, 0, 0, 1, 1, 0, 0)): '║',
+                    str((1, 1, 0, 0, 0, 1, 0, 0)): '║',
+                    str((0, 1, 0, 0, 0, 1, 1, 0)): '║',
+                    str((0, 0, 0, 1, 1, 1, 0, 1)): '┐',
+                    str((0, 0, 0, 0, 0, 1, 1, 1)): '┐',
+                    str((1, 1, 1, 1, 1, 1, 0, 1)): '┐',
+                    str((0, 1, 1, 1, 0, 1, 0, 0)): '┐',
+                    str((1, 1, 0, 1, 0, 0, 0, 1)): '└',
+                    str((0, 1, 1, 1, 0, 0, 0, 0)): '└',
+                    str((0, 1, 0, 1, 1, 1, 0, 0)): '└',
+                    str((1, 1, 0, 1, 1, 1, 1, 1)): '└',
+                    str((0, 1, 1, 1, 0, 0, 0, 1)): '┘',
+                    str((1, 1, 0, 0, 0, 0, 0, 1)): '┘',
+                    str((0, 1, 1, 1, 1, 1, 1, 1)): '┘',
+                    str((0, 0, 0, 1, 0, 1, 1, 1)): '┌',
+                    str((0, 0, 0, 1, 1, 1, 0, 0)): '┌',
+                    str((1, 1, 1, 1, 0, 1, 1, 1)): '┌',
+                    str((1, 1, 1, 0, 0, 1, 1, 1)): '│',
+                    str((1, 1, 1, 1, 1, 1, 0, 0)): '│',
+                    str((0, 1, 1, 1, 1, 1, 0, 0)): '│',
+                    str((1, 1, 0, 0, 0, 1, 1, 1)): '│',
+                    str((1, 1, 0, 0, 1, 1, 1, 1)): '│',
+                    str((0, 1, 1, 1, 1, 1, 1, 0)): '│',
+                    str((0, 0, 0, 1, 1, 1, 1, 1)): '─',
+                    str((1, 1, 1, 1, 0, 0, 0, 1)): '─',
+                    str((1, 1, 1, 1, 0, 0, 1, 1)): '─',
+                    str((1, 1, 1, 1, 1, 0, 0, 1)): '─',
+                    str((1, 0, 0, 1, 1, 1, 1, 1)): '─',
+                    str((0, 0, 1, 1, 1, 1, 1, 1)): '─',
+                    }
+        for x in range(len(cells)):
+            for y in range(len(cells[0])):
+                if cells[x][y] != Cell['WALL']:
+                    continue
+                mat = [[cells[x][y] == Cell['WALL'] if 0 <= x < len(cells) and 0 <= y < len(cells[0]) else False for y in range(y - 1, y + 2)] for x in range(x - 1, x + 2)]
+                neighbors = str((int(mat[0][0]),
+                                 int(mat[1][0]),
+                                 int(mat[2][0]),
+                                 int(mat[2][1]),
+                                 int(mat[2][2]),
+                                 int(mat[1][2]),
+                                 int(mat[0][2]),
+                                 int(mat[0][1])))
+                if not str(neighbors) in wall_map.keys():
+                    out[x][y] = "#"
+                else:
+                    out[x][y] = wall_map[str(neighbors)]
+        return out
