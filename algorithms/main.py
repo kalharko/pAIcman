c30cd5de9fff849d77d7613ef59dad933d0786f8
kalharko <oscar.dewasmes@gmail.com>
2023-10-23 12:27:30 +0200

    :tada: simulation cycle

--- algorithms/main.py
+++ algorithms/main.py
@@ -1,18 +1,27 @@
 from algorithms.perception import Perception
+from back.agent import Agent
 from back.pacman_game import PacmanGame
 from back.pacman import Pacman
 from back.ghost import Ghost
 from utils.board import Board
+from algorithms.pacman_brain import PacmanBrain
+from algorithms.ghost_brain import GhostBrain
 
 
 class Main():
-    team_a_perception: Perception
-    team_b_perception: Perception
+    perception_team_a: Perception
+    perception_team_b: Perception
+    id_team_a: tuple[str]
+    id_team_b: tuple[str]
+    brain_pacman: PacmanBrain
+    brain_ghost: GhostBrain
     environment: PacmanGame
 
     def __init__(self) -> None:
-        self.team_a_perception = Perception()
-        self.team_b_perception = Perception()
+        self.perception_team_a = Perception()
+        self.perception_team_b = Perception()
+        self.id_team_a = ('Pa', 'Ga1', 'Ga2', 'Ga3')
+        self.id_team_b = ('Pb', 'Gb1', 'Gb2', 'Gb3')
 
         # set up environment
         self.environment = PacmanGame()
@@ -29,7 +38,30 @@ class Main():
 
     def run(self) -> None:
         # simulation cycle
-        state = self.environment.gather_state()
+        # gather state
+        board, agents = self.environment.gather_state()
 
-    def mask_perception(self, board: Board) -> None:
+        # mask perception for each agents
+        ta_update = []
+        tb_update = []
+        for agent in agents:
+            update = self.mask_perception(board, agent, agents)
+            if agent.get_id() in self.id_team_a:
+                ta_update.append(update)
+            else:
+                tb_update.append(update)
+
+        # add to each team's perception
+        for perception in ta_update:
+            self.perception_team_a.update(perception)
+        for perception in tb_update:
+            self.perception_team_b.update(perception)
+
+        # compute strategy
+
+        # send strategy and perception to agents to compute their action
+
+        # apply to environment
+
+    def mask_perception(self, board: Board, agent: Agent, agents: list[Agent]) -> Perception:
         pass
--- algorithms/perception.py
+++ algorithms/perception.py
@@ -1,6 +1,18 @@
 from utils.board import Board
-
+from copy import deepcopy
 
 class Perception:
-    def __init__(self):
+    _board: Board
+    _last_seen: dict[str, int]
+
+    def __init__(self, board: Board, agent_ids: list[str]) -> None:
+        assert isinstance(board, Board)
+        assert isinstance(agent_ids, list)
+        assert len(agent_ids) > 0
+        assert isinstance(agent_ids[0], str)
+
+        self._board = deepcopy(board)
+        self._last_seen = {id: None for id in agent_ids}
+
+    def update(self, other: 'Perception') -> None:
         pass
