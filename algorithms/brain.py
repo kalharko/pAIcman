df562e4b3d441d24c1844bac62aaea490d288bab
Florian CLOAREC <39800745+Flo3171@users.noreply.github.com>
2023-12-06 19:55:56 +0100

    implement heritance for pacman_brain.py and ghost_brain.py with brain.py

--- algorithms/brain.py
+++ algorithms/brain.py
@@ -1,4 +1,122 @@
-
+import random
+import copy
+from back.agent_manager import AgentManager
+from back.perception import Perception
+from utils.action import Action
+from utils.direction import Direction
+from utils.strategy import Strategy
+from utils.replay_logger import ReplayLogger
+from back.cell import Cell
+from back.team import Team
 
 class Brain():
-    pass
+    _agentManager: AgentManager
+    _already_visited: list[tuple[int, int]]
+    _last_cell_visited: tuple[int, int]
+    # hyper parameters
+    # TODO: hyper parameters
+    _EXPLORATION_FORGETHING_RATE: float
+    _EXPLORATION_PAC_GUM_SCORE: float
+    _EXPLORATION_PAC_DOT_SCORE: float
+    _EXPLORATION_UNKNOWN_CELL_SCORE: float
+    _EXPLORATION_LAST_CELL_VISITED_SCORE: float
+
+    def __init__(self, agent_manager):
+        self._agentManager = agent_manager
+        self._already_visited = None
+        self._last_visited = None
+
+    def compute_action(self, strategy: Strategy, team: Team, agent_id: str) -> Action:
+        """Decision-making of a pacman agent for a given strategy
+
+        :param strategy: strategy that the pacman agent will apply
+        :type strategy: Strategy
+        :param perception: the team perception that the pacman agent will use to make its decision
+        :type perception: Perception
+        :param agent_id: id of the pacman agent
+        :type agent_id: str
+        :return: the optimal action for the pacman agent for the given strategy
+        :rtype: Action
+        """
+        assert isinstance(strategy, Strategy)
+        assert isinstance(team, Team)
+        assert isinstance(agent_id, str)
+
+        if strategy == Strategy['RANDOM']:
+            return Action(agent_id, random.choice(list(Direction)))
+
+        if strategy == Strategy['EXPLORATION']:
+            return self._exploration(team, agent_id)
+
+        if strategy == Strategy['AGRESSION']:
+            return self._agression(team, agent_id)
+
+        if strategy == Strategy['DEFENSE']:
+            return self._defense(team, agent_id)
+
+    def _exploration(self, team: Team, agent_id: str) -> Action:
+        """Give the best exploration action for the given agent
+
+        :param perception: the team perception that the pacman agent will use to make its decision
+        :type perception: Perception
+        :param agent_id: id of the pacman agent
+        :type agent_id: str
+        :return: the optimal exploration action for the given agent
+        :rtype: Action
+        """
+
+        bestScore = None
+        chosenDirection = None
+        ReplayLogger().log_comment('\n' + agent_id)
+        perception = team.get_perception()
+        opposite_dir = team.get_pacman().get_last_direction()
+        opposite_dir = Direction((opposite_dir.value[0] * -1, opposite_dir.value[1] * -1))
+        self._last_cell_visited = perception.get_board().get_next_cell(team.get_pacman().get_position(), opposite_dir)
+        for direction in self.get_legal_move(perception, agent_id):  # TODO : shuffle the order of move
+            self._already_visited = [team.get_pacman().get_position()]
+            exploration_score = self.get_exploration_score(perception, agent_id, self.get_agent_position(agent_id), direction)
+            ReplayLogger().log_comment(str(direction) + ' ' + str(exploration_score))
+            if bestScore == None or exploration_score >= bestScore:
+                chosenDirection = direction
+                bestScore = exploration_score
+
+        #print(chosenDirection)
+        return Action(agent_id, chosenDirection)
+
+    def get_exploration_score(self, perception: Perception, agent_id: str, position: (int, int), direction: Direction) -> float:
+        next_cell = perception.get_board().get_next_cell(position, direction)
+
+        # stop case 1 : if is next cell non-legal
+        if not perception.get_board().get_cell(position).is_movable():
+            return 0.0
+
+        # stop case 2 : if the cell is already visited
+        elif next_cell in self._already_visited:
+            return 0.0
+
+        # stop case 3 : if is next cell non-visible
+        elif perception.get_board().get_cell(next_cell) == Cell['UNKNOWN']:
+            return self._EXPLORATION_UNKNOWN_CELL_SCORE
+
+        # recursive case : continue in  all other direction
+        else:
+            score = 0
+            if perception.get_board().get_cell(next_cell) == Cell['PAC_GUM']:
+                score = self._EXPLORATION_PAC_GUM_SCORE
+            elif perception.get_board().get_cell(next_cell) == Cell['PAC_DOT']:
+                score = self._EXPLORATION_PAC_DOT_SCORE
+
+            if next_cell == self._last_cell_visited:
+                score = self._EXPLORATION_LAST_CELL_VISITED_SCORE
+            self._already_visited.append(copy.copy(next_cell))
+            for direction in Direction:
+                score += self.get_exploration_score(perception, agent_id, next_cell, direction)
+            return score * self._EXPLORATION_FORGETHING_RATE
+
+    def get_legal_move(self, perception: Perception, agent_id: str) -> list[Direction]:
+        return perception.get_board().get_legal_move(self.get_agent_position(agent_id))
+
+    def get_agent_position(self, agent_id: str) -> (int, int):
+        return self._agentManager.get_agent(agent_id).get_position()
+
+
--- algorithms/ghost_brain.py
+++ algorithms/ghost_brain.py
@@ -11,106 +11,15 @@ from back.team import Team
 
 
 class GhostBrain(Brain):
-    _agentManager: AgentManager
     def __init__(self, agent_manager):
-        self._agentManager = agent_manager
+        super().__init__(agent_manager)
 
-    def compute_action(self, strategy: Strategy, team: Team, id: str) -> Action:
-        """Decision making of a ghost agent for a given strategy
-
-        :param strategy: strategy that the ghost agent will apply
-        :type strategy: Strategy
-        :param perception: the team perception that the ghost agent will use to make it's decision
-        :type perception: Perception
-        :param id: id of the ghost agent
-        :type id: str
-        :return: the optimal action for the ghost agent for the given strategy
-        :rtype: Action
-        """
-        assert isinstance(strategy, Strategy)
-        assert isinstance(team, Team)
-        assert isinstance(id, str)
-
-        if strategy == Strategy['RANDOM']:
-            return Action(id, random.choice(list(Direction)))
-
-        if strategy == Strategy['EXPLORATION']:
-            return Action(id, random.choice(list(Direction)))
-
-        if strategy == Strategy['AGRESSION']:
-            return self._aggression(team, id)
-            # return Action(id, random.choice(list(Direction)))
-
-        if strategy == Strategy['DEFENSE']:
-            return Action(id, random.choice(list(Direction)))
-
-    def _exploration(self, perception: Perception, agent_id: str) -> Action:
-        """Give the best exploration action for the given agent
-
-        :param perception: the team perception that the ghost agent will use to make it's decision
-        :type perception: Perception
-        :param agent_id: id of the ghost agent
-        :type agent_id: str
-        :return: the optimal exploration action for the given agent
-        :rtype: Action
-        """
-        bestScore = 0
-        chosenDirection = Direction.NONE
-        for direction in self.get_legal_move(perception, agent_id):  # TODO : shuffle the order of move
-            if self.get_exploration_score(perception, agent_id, self.get_agent_position(agent_id), direction,
-                                          []) >= bestScore:
-                chosenDirection = direction
-
-        return Action(agent_id, chosenDirection)
-
-    def get_exploration_score(self, perception: Perception, agent_id: str, position: (int, int), direction: Direction,
-                              already_visited: [(int, int)]) -> float:
-        forgetting_rate = 0.5
-        next_cell = perception.get_board().get_next_cell(position, direction)
-
-        # stop case 1 : if is next cell non-legal
-        if not perception.get_board().get_cell(position).is_movable():
-            return 0.0
-
-        # stop case 2 : if the cell is already visited
-        elif already_visited.search(next_cell):
-            return 0.0
-
-        # stop case 2 : if is next cell non-visible
-        elif not perception.is_visible(next_cell, agent_id):
-            return 1.0
-
-        # recursive case : continue in  all other direction
-        else:
-            score = 0
-            already_visited.append(next_cell)
-            for direction in Direction:
-                score += self.get_exploration_score(perception, agent_id, next_cell, direction, already_visited)
-            return forgetting_rate * score
-
-    def _agression(team: Team, agent_id: str) -> Action:
-        """Give the best agressive action for the given agent
-
-        :param team: the team perception that the ghost agent will use to make it's decision
-        :type team: Team
-        :param agent_id: id of the ghost agent
-        :type agent_id: str
-        :return: the optimal agressive action for the given agent
-        :rtype: Action
-        """
-
-        # init a_star
-        a_star = AStar(team.get_perception().get_board())
-
-        # check if we have a sighting for the enemy pacman
-        pacman_sighting = team.get_perception().get_pacman_sightings()
-        if pacman_sighting is None:
-            return Action(agent_id, random.choice(list(Direction)))
-
-        # get the action that brings the ghost closer to the last pacman sighting
-        pacman_position = (pacman_sighting[1], pacman_sighting[2])
-        direction = a_star.first_step_of_path(team.get_agent(agent_id), pacman_position)
-        return Action(agent_id, direction)
+        # define hyper parameters
+        self._EXPLORATION_FORGETHING_RATE = 0.5
+        self._EXPLORATION_PAC_GUM_SCORE = 0
+        self._EXPLORATION_PAC_DOT_SCORE = 0
+        self._EXPLORATION_UNKNOWN_CELL_SCORE = 1
+        self._EXPLORATION_LAST_CELL_VISITED_SCORE = -5
 
     def _defense(perception: Perception, agent_id: str) -> Action:
         """Give the best defensive action for the given agent
@@ -123,9 +32,3 @@ class GhostBrain(Brain):
         :rtype: Action
         """
         return Action(agent_id, random.choice(list(Direction)))
-
-    def get_legal_move(self, perception: Perception, agent_id: str) -> list[Direction]:
-        return perception.get_board().get_legal_move(self.get_agent_position(agent_id))
-
-    def get_agent_position(self, agent_id: str) -> (int, int):
-        return self._agentManager.get_agent(agent_id).get_position()
