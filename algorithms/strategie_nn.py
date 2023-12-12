371ac89fe395b587f949c5aa1229ac9ef6875e37
Florian CLOAREC <39800745+Flo3171@users.noreply.github.com>
2023-12-12 18:23:49 +0100

    :sparkles: strategie_nn.py

---
+++ algorithms/strategie_nn.py
@@ -0,0 +1,29 @@
+from back.perception import Perception
+from back.team import Team
+from utils.action import Action
+
+
+class StrategieNN() :
+    def __init__(self) -> None:
+        # TODO: load cerveau
+        pass
+
+
+    def compute(self, team: Team) -> list[Action]:
+        """
+        compute the best strategie policie for a team given a team perception
+        :param team:
+        :return:
+        """
+
+
+        pass
+
+    # TODO: transformer la team en input du réseau
+
+    def train(self, nb_epoch):
+        #TODO: train
+        pass
+
+
+
