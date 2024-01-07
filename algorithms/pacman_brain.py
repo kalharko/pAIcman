884156fcb69af016db54e413b05d11879a6a70eb
hermidrn <hermance.dreano@utbm.fr>
2024-01-07 14:46:31 +0100

    behavior pacman agression

--- algorithms/pacman_brain.py
+++ algorithms/pacman_brain.py
@@ -1,9 +1,12 @@
 import random
 
-from algorithms.brain import Brain
+from brain import Brain
 from back.perception import Perception
+from back.team import Team
+from back.cell import Cell
 from utils.action import Action
 from utils.direction import Direction
+from a_star import AStar
 
 
 class PacmanBrain(Brain):
@@ -18,7 +21,7 @@ class PacmanBrain(Brain):
         self._EXPLORATION_UNKNOWN_CELL_SCORE = 1
         self._EXPLORATION_LAST_CELL_VISITED_SCORE = -5
 
-    def _agression(perception: Perception, agent_id: str) -> Action:
+    def _agression(perception: Perception, agent_id: str, team: Team) -> Action:
         """Give the best agressive action for the given agent
 
         :param perception: the team perception that the pacman agent will use to make its decision
@@ -28,7 +31,34 @@ class PacmanBrain(Brain):
         :return: the optimal agressive action for the given agent
         :rtype: Action
         """
-        return Action(agent_id, random.choice(list(Direction)))
+        a_star = AStar(team.get_perception().get_board())
+        pacman_sightings = team.get_perception().get_sightings()
+
+        ghost_seen = False
+        pacgum_activated = False
+        ghost_position = None
+
+        for id, sighting in pacman_sightings.items():
+            if "g" in id:
+                ghost_seen = True
+                ghost_position = (sighting[1], sighting[2])
+            elif team.get_perception().get_board().get_cell(sighting[1], sighting[2]) == Cell.PAC_GUM and sighting[0] == 0:
+                pacgum_activated = True
+
+        if not ghost_seen:
+            return Action(agent_id, random.choice(list(Direction)))
+
+        if pacgum_activated:
+            #if a pacgum is activated, pacman will try to prioritize attacking the ghost
+
+            ghosts = team.get_perception().get_ghost_ids() #get all the ghost the team can see currently 
+            if ghosts:
+                nearest_ghost = min(ghosts, key=lambda ghost: a_star.distance(ghost, ghost_position)) #get the nearest ghost by checking the path length between the ghost and the pacman 
+                direction = a_star.first_step_of_path(team.get_agent(agent_id), nearest_ghost) #follow the path to the ghost 
+                return Action(agent_id, direction)
+        else:
+            direction = a_star.first_step_of_path(team.get_agent(agent_id), ghost_position)
+            return Action(agent_id, direction)
 
     def _defense(perception: Perception, agent_id: str) -> Action:
         """Give the best defensive action for the given agent
